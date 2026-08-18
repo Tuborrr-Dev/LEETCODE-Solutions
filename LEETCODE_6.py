4c7bbc34e684fad87516f1a0a4ff4a018ba9abe3
Tuborrr-Dev <Israeltubo@gmail.com>
2026-08-19 00:00:15 +0100

    Well that's a wrap

--- LEETCODE_6.py
+++ LEETCODE_6.py
@@ -2,7 +2,7 @@ class Solution:
     def reverse(self, x: int) -> int:
         # first assumption is using strings and reversing but if the number
         # were to start with 0 if revevrsed how do we handles that
-        while -(2**31) <= x <= 2**31 - 1:
+        if ((-(2**31))) <= x <= 2**31 - 1:
             digits = []
             result = 0
             is_negative = False
@@ -13,8 +13,6 @@ class Solution:
                 digits.insert(0, x % 10)  # Gets the last digit and insert at the front
                 x //= 10
             for index in range(len(digits) - 1, -1, -1):
-                print(digits[index])
-                print(index)
                 # now i use the scope computers use in calculating bits
                 # use the index to create indices
                 if digits[index] is 0:
@@ -23,6 +21,9 @@ class Solution:
                     result += (10**index) * digits[index]
             if is_negative:
                 result *= -1
-            return result
+            if ((-(2**31))) <= result <= (2**31) - 1:
+                return result
+            else:
+                return 0
         else:
             return 0
